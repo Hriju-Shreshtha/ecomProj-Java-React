539810a5f08ebc5a333e9a67e084ce740ab6f762
Hriju Shreshtha <hrijushreshtha11@gmail.com>
2025-07-18 23:49:57 +0530

    Initial commit: React + Spring Boot E-commerce project

---
+++ ecom-backend/src/main/java/com/example/ecom_proj/service/ProductService.java
@@ -0,0 +1,35 @@
+package com.example.ecom_proj.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.ecom_proj.model.Product;
+import com.example.ecom_proj.repo.ProductRepo;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepo repo;
+
+	public List<Product> getAllProducts() {
+		return repo.findAll();
+	}
+
+	public Product getProductById(int id) {
+//		return repo.findById(id).orElse(new Product());
+		return repo.findById(id).orElse(null);
+	}
+
+	public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+		product.setImageName(imageFile.getOriginalFilename());
+		product.setImageType(imageFile.getContentType());
+		product.setImageData(imageFile.getBytes());
+		return repo.save(product);
+	}
+
+}
